5e01580cbe9b264163ce1615015af01513e3bc53
Dmytro Yantsybaiev <d.yantsybaiev@icloud.com>
2021-12-30 20:08:08 +0200

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Dmytro Yantsybaiev on 30.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
